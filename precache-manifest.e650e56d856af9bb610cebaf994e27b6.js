5634ecf81232e4a45d805cb0e449e60848c9f936
BernardoKaluza <bernardokaluza@ua.pt>
2022-06-01 00:36:24 +0100

    Updates

---
+++ precache-manifest.e650e56d856af9bb610cebaf994e27b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "660cfb782a80bd836a1e",
+    "url": "/static/css/main.3d4ce07a.chunk.css"
+  },
+  {
+    "revision": "660cfb782a80bd836a1e",
+    "url": "/static/js/main.660cfb78.chunk.js"
+  },
+  {
+    "revision": "3596aba066ef29cec558",
+    "url": "/static/js/1.3596aba0.chunk.js"
+  },
+  {
+    "revision": "13b1d79f3abdba641e93",
+    "url": "/static/css/2.99831bc7.chunk.css"
+  },
+  {
+    "revision": "13b1d79f3abdba641e93",
+    "url": "/static/js/2.13b1d79f.chunk.js"
+  },
+  {
+    "revision": "fc9579aff5af8ebf1522",
+    "url": "/static/js/runtime~main.fc9579af.js"
+  },
+  {
+    "revision": "999222d8a5bbf1e6306528ab6116b9c1",
+    "url": "/static/media/MACO_Logo.999222d8.png"
+  },
+  {
+    "revision": "802f160a48ef08e445ed5527df4eba5a",
+    "url": "/static/media/avatar.802f160a.jpg"
+  },
+  {
+    "revision": "ee3c68c7158ae9a6e7a212d68a1f61d2",
+    "url": "/index.html"
+  }
+];
