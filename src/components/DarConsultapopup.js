60468ff1bd2e0574ea731c460c10dd026c258d6f
BernardoKaluza <bernardokaluza@sapo.pt>
2022-06-01 10:25:15 +0100

    Update DarConsultapopup.js

--- src/components/DarConsultapopup.js
+++ src/components/DarConsultapopup.js
@@ -9,6 +9,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import { useState } from 'react'
 
 
+
 import Autocomplete from '@mui/material/Autocomplete';
 
 import {utentes} from '../data/utentes.js'
@@ -45,11 +46,13 @@ export default function MarcarConsultaPopup(props) {
     // localStorage.setItem('NumeroUtente',NumeroUtente)
     // localStorage.setItem('Notas',Notas)
     //window.location.reload(false);
+   
     
     setOpen(false);
     if (props.submit!=null) props.submit(Notas)
     if (props.close!=null) props.close()
   }
+
 
 
 
@@ -59,99 +62,109 @@ export default function MarcarConsultaPopup(props) {
         Marcar Consulta
       </Button> */}
       <Dialog open={open} onClose={handleClose}>
-        
         <DialogContent>
-          <form noValidate autoComplete="off" >
-          <Autocomplete
-            sx={{width:'100%', alignItems:'center', justifyContent:'center',paddingTop:'0.5vw',paddingBottom:'0.25vw'}}
-            disabled
-            defaultValue={props.descricao}
-            freeSolo
-            id="Titulo"
-            disableClearable
-            options={utentes.map((option) => option.Name)}
-            renderInput={(params) => (
-              <TextField
-                variant ="filled"
-                multiline
-                rows={4}
+          <form noValidate autoComplete="off">
+            <Autocomplete
+              sx={{
+                width: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+                paddingTop: "0.5vw",
+                paddingBottom: "0.25vw",
+              }}
+              disabled
+              defaultValue={props.descricao}
+              freeSolo
+              id="Titulo"
+              disableClearable
+              options={utentes.map((option) => option.Name)}
+              renderInput={(params) => (
+                <TextField
+                  variant="filled"
+                  multiline
+                  rows={4}
+                  {...params}
+                  label="Descricao"
+                  InputProps={{
+                    ...params.InputProps,
+                    type: "search",
+                  }}
+                />
+              )}
+            />
+            <Autocomplete
+              sx={{
+                width: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+                paddingTop: "0.25vw",
+                paddingBottom: "0.25vw",
+              }}
+              freeSolo
+              disabled
+              defaultValue={props.paciente}
+              id="Search Name"
+              disableClearable
+              options={utentes.map((option) => option.Name)}
+              renderInput={(params) => (
+                <TextField
+                  variant="filled"
+                  onChange={(e) => setnomeUtente(e.target.value)}
+                  {...params}
+                  label="Nome Utente"
+                  InputProps={{
+                    ...params.InputProps,
+                    type: "search",
+                  }}
+                />
+              )}
+            />
+            <Autocomplete
+              sx={{
+                width: "100%",
+                paddingTop: "0.25vw",
+                paddingBottom: "0.25vw",
+              }}
+              disabled
+              defaultValue={"999080114"}
+              freeSolo
+              id="Search Name"
+              disableClearable
+              options={utentes.map((option) => option.ID)}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  disabled
+                  variant="filled"
+                  onChange={(e) => setNumeroUtente(e.target.value)}
+                  label="Numero de Utente"
+                  InputProps={{
+                    ...params.InputProps,
+                    type: "search",
+                  }}
+                />
+              )}
+            />
 
-                {...params}
-                label="Descricao"
-                InputProps={{
-                  ...params.InputProps,
-                  type: 'search',
-                }}
-              />
-            )}
-          />
-          <Autocomplete
-            sx={{width:'100%', alignItems:'center', justifyContent:'center',paddingTop:'0.25vw',paddingBottom:'0.25vw'}}
-            freeSolo
-            disabled
-            defaultValue={props.paciente}
-            id="Search Name"
-            disableClearable
-            options={utentes.map((option) => option.Name)}
-            renderInput={(params) => (
-              <TextField
-                variant ="filled"
-                onChange={(e) => setnomeUtente(e.target.value)}
-
-                {...params}
-                label="Nome Utente"
-                InputProps={{
-                  ...params.InputProps,
-                  type: 'search',
-                }}
-              />
-            )}
-          />
-          <Autocomplete
-            sx={{width:'100%',paddingTop:'0.25vw',paddingBottom:'0.25vw'}}
-            disabled
-            defaultValue={"999080114"}
-            freeSolo
-            id="Search Name"
-            disableClearable
-            options={utentes.map((option) => option.ID)}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                disabled
-                variant="filled"
-                onChange={(e) => setNumeroUtente(e.target.value)}
-
-                label="Numero de Utente"
-                InputProps={{
-                  ...params.InputProps,
-                  type: 'search',
-                }}
-              />
-            )}
-          />
-
-          <TextField
-            sx ={{
-              paddingTop:'0.25vw',
-              display:'flex',
-            }}
-
-            onChange={(e) => setNotas(e.target.value)}
-            multiline
-            rows={4}
-            label="Notas da consulta"
-
-          />
-
+            <TextField
+              sx={{
+                paddingTop: "0.25vw",
+                display: "flex",
+              }}
+              onChange={(e) => setNotas(e.target.value)}
+              multiline
+              rows={4}
+              label="Notas da consulta"
+            />
           </form>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCloseSend}>Submeter</Button>
           <Button onClick={handleClose}>Cancelar</Button>
-          <Button onClick={handleApagar}>Apagar</Button>
+          <Button onClick={handleApagar}>Desmarcar</Button>
         </DialogActions>
       </Dialog>
+
     </div>
   );
 
