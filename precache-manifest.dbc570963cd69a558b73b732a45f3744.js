c015a54427c49a4a784039695f429d34ef15deb5
BernardoKaluza <bernardokaluza@ua.pt>
2022-06-08 21:54:25 +0100

    Updates

---
+++ precache-manifest.dbc570963cd69a558b73b732a45f3744.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c0f90bb9ecc3fe07a6b",
+    "url": "/maco/static/css/main.814b020e.chunk.css"
+  },
+  {
+    "revision": "0c0f90bb9ecc3fe07a6b",
+    "url": "/maco/static/js/main.0c0f90bb.chunk.js"
+  },
+  {
+    "revision": "c91f8be63e4f9e6859aa",
+    "url": "/maco/static/js/1.c91f8be6.chunk.js"
+  },
+  {
+    "revision": "0e294c95e3ad2a8db003",
+    "url": "/maco/static/css/2.b8ed1608.chunk.css"
+  },
+  {
+    "revision": "0e294c95e3ad2a8db003",
+    "url": "/maco/static/js/2.0e294c95.chunk.js"
+  },
+  {
+    "revision": "ee6e23cee69f7a2d42dc",
+    "url": "/maco/static/js/runtime~main.ee6e23ce.js"
+  },
+  {
+    "revision": "999222d8a5bbf1e6306528ab6116b9c1",
+    "url": "/maco/static/media/MACO_Logo.999222d8.png"
+  },
+  {
+    "revision": "b7b93057dc548ab2a4358cd0176f91e4",
+    "url": "/maco/static/media/help.b7b93057.gif"
+  },
+  {
+    "revision": "802f160a48ef08e445ed5527df4eba5a",
+    "url": "/maco/static/media/avatar.802f160a.jpg"
+  },
+  {
+    "revision": "a12339e42851b45a04c412d5b5f2d217",
+    "url": "/maco/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/maco/precache-manifest.d8e957e441fab54633113cebd35c9bee.js"
+  "/maco/precache-manifest.dbc570963cd69a558b73b732a45f3744.js"
 );
 
 workbox.clientsClaim();
